98cbbf21546abdaf0c8882655da9d261ecd66887
ridhoafni-dev <ridhoafni.dev@gmail.com>
2026-07-21 21:39:27 +0700

    Initial Commit

---
+++ IosFundamentalSubmission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosFundamentalSubmission
+//
+//  Created by User on 21/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
